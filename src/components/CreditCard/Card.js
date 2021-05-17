0508b921b878360f625f8b661ab87e94c880c3fa
meti651 <meti651@gmail.com>
2021-05-17 21:08:20 +0200

    credit card base design

--- src/components/CreditCard/Card.js
+++ src/components/CreditCard/Card.js
@@ -7,16 +7,19 @@ export default function Card({ cardData }) {
         <div id={Styles.card_container}>
             <div id={Styles.card_inner}>
                 <div id={Styles.card_front}>
+                    <div id={Styles.placeholder}></div>
                     <div id={Styles.icon}></div>
-                    <div id={Styles.cardNum}></div>
-                    <div id={Styles.cardBottom}>
-                        <div id={Styles.name}></div>
-                        <div id={Styles.expiration}></div>
+                    <div id={Styles.card_num}>{cardData.cardNum}</div>
+                    <div id={Styles.card_bottom}>
+                        <div id={Styles.name}>{cardData.name}</div>
+                        <div id={Styles.expiration}>
+                            {cardData.expirationDate.month}/{cardData.expirationDate.year.slice(2)}
+                        </div>
                     </div>
                 </div>
                 <div id={Styles.card_back}>
                     <div id={Styles.black_line}></div>
-                    <div id={Styles.cvv}></div>
+                    <div id={Styles.cvv}>{cardData.cvv}</div>
                 </div>
             </div>
         </div>
