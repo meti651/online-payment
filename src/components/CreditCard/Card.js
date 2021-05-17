282dfaa938cab10358d6633e697c0748bff3afd7
meti651 <meti651@gmail.com>
2021-05-17 19:24:53 +0200

    card front design

--- src/components/CreditCard/Card.js
+++ src/components/CreditCard/Card.js
@@ -2,8 +2,21 @@ import React from "react";
 
 import * as Styles from "./Card.module.scss";
 
-export default function Card() {
-    return <div>
-        
+export default function Card({cardData}) {
+    return <div id={Styles.card_container}>
+        <div id={Styles.icon}>
+
+        </div>
+        <div id={Styles.cardNum}>
+
+        </div>
+        <div id={Styles.cardBottom}>
+            <div id={Styles.name}>
+
+            </div>
+            <div id={Styles.expiration}>
+
+            </div>
+        </div>
     </div>;
 }
