5aa28a45c63150fc25411f22ccfbd64c4cc5e488
meti651 <meti651@gmail.com>
2021-05-17 15:46:27 +0200

    form base structure

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,26 +9,65 @@ export default function Form() {
         name: "",
         expirationDate: {
             month: "",
-            year: ""
+            year: "",
         },
         cvv: "",
-        phoneNum: ""
-    })
+        phoneNum: "",
+    });
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
-    }
+    };
 
-    const handleInputChange = ({target}) => {
-        setFormData(data => ({ ...data, [target.name]: target.value}));
-    }
+    const handleInputChange = ({ target }) => {
+        setFormData((data) => ({ ...data, [target.name]: target.value }));
+    };
 
-    return <div>
-        <div id={Styles.credit_card_wrapper}>
-            <Card cardData={formData} />
+    const handleExpirationDateChange = ({ target }) => {
+        setFormData((data) => ({ ...data, expirationDate: { [target.name]: target.value } }));
+    };
+
+    return (
+        <div>
+            <div id={Styles.credit_card_wrapper}>
+                <Card cardData={formData} />
+            </div>
+            <form onSubmit={handleFormSubmit}>
+                <div className="container">
+                    <div className="row">
+                        <div className="column">
+                            <label htmlFor="cardNum">Kártyaszám</label>
+                            <input name="cardNum" value={formData.cardNum} onChange={handleInputChange} required />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="column">
+                            <label htmlFor="name">Kártyabirtokos neve</label>
+                            <input name="name" value={formData.name} onChange={handleInputChange} required />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="column">
+                            <label>Lejárati dátum</label>
+                            <input name="month" value={formData.month} onChange={handleExpirationDateChange} required />
+                            <input name="year" value={formData.year} onChange={handleExpirationDateChange} required />
+                        </div>
+                        <div className="column">
+                            <label htmlFor="cvv">CVV</label>
+                            <input name="cvv" value={formData.cvv} onChange={handleInputChange} required />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="column">
+                            <label htmlFor="phoneNum">Telefonszám</label>
+                            <input name="phoneNum" value={formData.phoneNum} onChange={handleInputChange} required />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <button>Küldés</button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <form onSubmit={handleFormSubmit}>
-            
-        </form>
-    </div>;
+    );
 }
