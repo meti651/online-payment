e892badb2461827598a513af4ca6df2981864733
meti651 <meti651@gmail.com>
2021-05-17 19:45:26 +0200

    flip card base

--- src/components/CreditCard/Card.js
+++ src/components/CreditCard/Card.js
@@ -2,21 +2,23 @@ import React from "react";
 
 import * as Styles from "./Card.module.scss";
 
-export default function Card({cardData}) {
-    return <div id={Styles.card_container}>
-        <div id={Styles.icon}>
-
-        </div>
-        <div id={Styles.cardNum}>
-
-        </div>
-        <div id={Styles.cardBottom}>
-            <div id={Styles.name}>
-
+export default function Card({ cardData }) {
+    return (
+        <div id={Styles.card_container}>
+            <div id={Styles.card_inner}>
+                <div id={Styles.card_front}>
+                    <div id={Styles.icon}></div>
+                    <div id={Styles.cardNum}></div>
+                    <div id={Styles.cardBottom}>
+                        <div id={Styles.name}></div>
+                        <div id={Styles.expiration}></div>
+                    </div>
+                </div>
+                <div id={Styles.card_back}>
+                    <div id={Styles.black_line}></div>
+                    <div id={Styles.cvv}></div>
+                </div>
             </div>
-            <div id={Styles.expiration}>
-
-            </div>
         </div>
-    </div>;
+    );
 }
