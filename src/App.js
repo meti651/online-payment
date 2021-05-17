894992f0413dff0160f9f6df19499cd1576e6216
meti651 <meti651@gmail.com>
2021-05-17 15:46:03 +0200

    Form representation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+    return (
+        <div className="App">
+            <Form />
+        </div>
+    );
 }
 
 export default App;
