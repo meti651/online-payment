c6e5c01271153bac523e7fa7c0b4feb6f387d430
meti651 <meti651@gmail.com>
2021-05-17 21:40:34 +0200

    chip icon

--- src/components/CreditCard/Card.js
+++ src/components/CreditCard/Card.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import * as Styles from "./Card.module.scss";
 
+import Chip from "../../media/Chip.png";
+
 export default function Card({ cardData }) {
     return (
         <div id={Styles.card_container}>
             <div id={Styles.card_inner}>
                 <div id={Styles.card_front}>
                     <div id={Styles.placeholder}></div>
-                    <div id={Styles.icon}></div>
+                    <div id={Styles.icon}>
+                        <img src={Chip} alt="Chip" />
+                    </div>
                     <div id={Styles.card_num}>{cardData.cardNum}</div>
                     <div id={Styles.card_bottom}>
                         <div id={Styles.name}>{cardData.name}</div>
